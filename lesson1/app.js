64e30837d51bcb7be1711a5008948a786f737a43
Fatima <fatyaahmadova77@gmail.com>
2023-07-06 10:54:01 +0300

    Lesson2 finish

---
+++ lesson1/app.js
@@ -0,0 +1,22 @@
+// // number
+// varieble = value
+
+// console.log(12);
+// console.log(8.5);
+
+// //strings
+// console.log("My name is Zahid, I'm 23 years old");
+// console.log('My name is Zahid, I am 23 years old');
+
+// //booleans
+// console.log(true);
+// console.log(false);
+
+// // Null and underfined
+
+student1 = 'Elxan'
+student2 = 'Fatya'
+student3 = 'Jabbar'
+
+students = [student1, student2, student3]
+console.log(students);
---
+++ lesson2/app.js
@@ -0,0 +1,115 @@
+/*
+// typeof - value-nun datatype tapir
+console.log(typeof(true));
+console.log(typeof(5.2));
+console.log(typeof('mamed'));
+*/
+
+/*
+// Yazilis stili 
+
+// camelCase
+let macbookPrice = 36
+
+// snakelCase
+let macbook_Price_1 = 36
+
+// PascalCase
+let MacbookPrice = 36
+*/
+
+
+/*
+// Arifmetich operators
+let a = 30, b = 7
+
+// Addition (+) 
+let result1 = a + b
+console.log(`${a} + ${b} = ${result1}`);
+
+//   (-)
+let result2 = a - b
+console.log(`${a} - ${b} = ${result2}`);
+
+// Multiplication (*)
+let result3 = a * b
+console.log(`${a} * ${b} = ${result3}`);
+
+// Divide (/)
+let result4 = a / b
+console.log(`${a} / ${b} = ${result4}`);
+
+// Exponentiation (**)
+let result5 = a ** b
+console.log(`${a} ** ${b} = ${result5}`);
+
+
+// Modules (%) Qaliq
+let result6 = a % b //a-ni
+console.log(`${a} % ${b} = ${result6}`);
+*/
+
+/*
+let name = 'Fatima', age = 18
+console.log(`Hi my name is ${name}, I am ${age} years old`);
+*/  
+
+// Asigment Operators (beraberlesdirme)
+
+
+// +=  Soldaki ededi sagdaki qeder artir (soldaki eded deyisir)
+// a+=b  |  a ededini b qeder artir (a ededi deyisir/artir)
+// b+=a  |  b ededini a qeder artir (b ededi deyisir/artir)
+
+/*
+console.log(`a: ${a}`);
+console.log(`b: ${b}`);
+a+=b  // 22+=3  | 22 ededi 3 vahid artdi, 22 ededi deyisdi
+console.log('a+=b');
+console.log(`a: ${a}`);
+console.log(`b: ${b}`);
+*/
+
+/*
+console.log(`a: ${a}, b: ${b}`);
+a-=b  // 22-=3  | 22 ededi 3 vahid azaldi, 22 ededi deyisdi
+console.log('a-=b');
+console.log(`a: ${a}, b: ${b}`);
+*/
+
+/*
+console.log(`a: ${a}, b: ${b}`);
+a*=b  // 22*=3  | 22 ededi 3 defe artsin, 22 ededi deyisdi
+console.log('a*=b');
+console.log(`a: ${a}, b: ${b}`);
+
+console.log(`a: ${a} , b: ${b}`);
+b*=a  // 3*=22  | 3 ededi 22 defe artsin, 3 ededi deyisdi
+console.log('b*=a');
+console.log(`a: ${a} , b: ${b}`);
+*/
+
+/*
+console.log(`a: ${a} , b: ${b}`);
+a%=b 
+console.log('a%=b');
+console.log(`a: ${a} , b: ${b}`);
+*/
+
+/*
+let a = 5, b = 10
+console.log(`${a} < ${b} = ${a < b}`);
+console.log(`${a} > ${b} = ${a > b}`);
+*/
+
+
+
+
+
+
+
+
+
+
+
+
